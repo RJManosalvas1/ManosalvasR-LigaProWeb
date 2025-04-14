23d5bf2664b2a1bb921e95cc4190fa13894f60e7
rjmanosalvas <rjmanosalvas@gmail.com>
2025-04-13 21:43:55 -0500

    Agregar archivos de proyecto.

---
+++ ManosalvasR-LigaProWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManosalvasR_LigaProWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
