a74d0a624db7791cd3e45fe39c3f6cf24b215a63
rjmanosalvas <rjmanosalvas@gmail.com>
2025-04-13 21:58:02 -0500

    Prueba_2

---
+++ ManosalvasR-LigaProWeb/Models/Team.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ManosalvasR_LigaProWeb.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [DisplayName("Team Name: ")]
+        [Required]
+        [AllowNull]
+        public string Name { get; set; }
+        [Range(0, 20)]
+        public int PlayedGames { get; set; }
+        [Range(0, 20)]
+        public int WonGames { get; set; }
+        [Range(0, 20)]
+        public int LostGames { get; set; }
+        [Range(0, 20)]
+        public int TiedGames { get; set; }
+
+        public int Points
+        {
+            get
+            {
+                return (WonGames * 3) + TiedGames * 1;
+            }
+        }
+
+
+    }
+}
